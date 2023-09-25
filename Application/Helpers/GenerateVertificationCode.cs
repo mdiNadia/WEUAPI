00eb333cc625449bd5e1578339a5cebf078ae32f
Nadie <Nadie@weu.local>
2023-09-25 12:07:03 +0330

    change vertification Code to Num only

--- Application/Helpers/GenerateVertificationCode.cs
+++ Application/Helpers/GenerateVertificationCode.cs
@@ -11,8 +11,16 @@ namespace Application.Helpers
     {
         public static string CreateVertificationCode()
         {
-            var code = Guid.NewGuid().ToString("N").Substring(0, 7);
-            return code;
+            Random R = new Random();
+
+            double NUD_1Value = 1;
+            double NUD_2Value = 999999999999999; //15-digit number
+
+            var next = R.NextDouble();
+
+            double v = NUD_1Value + (next * (NUD_2Value - NUD_1Value));
+
+            return v.ToString().Substring(0, 4);
         }
     }
 }
